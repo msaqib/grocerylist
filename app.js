509ef0f5a2cbfabffede98ffbcf1831d91513cce
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-12-21 20:21:31 +0500

    Added a modal to confirm an item delete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.grocery')
 const newItem = document.getElementById('item')
 const submitButton = document.querySelector('.btn-add')
-// const itemContainer = document.querySelector('.grocery-container')
+const itemContainer = document.querySelector('.grocery-container')
 const list = document.querySelector('.grocery-list')
 // const clearButton = document.querySelector('.btn-clear')
 
@@ -35,6 +35,10 @@ function addItem(e) {
         editButton.onclick = editItem
         displayMessage(`${value} successfully added to the list`, 'success')
     }
+    if (list.children.length > 0) {
+        itemContainer.classList.add('show-groceries')
+    }
+    newItem.value = ''
 }
 
 function createArticle(name) {
@@ -65,10 +69,67 @@ function displayMessage(message, level) {
 
 function deleteItem(e) {
     const item = e.currentTarget.parentElement.parentElement
-    list.removeChild(item)
+    const value = item.querySelector('p').innerText
+    preModal()
+    showModal(`Are you sure you want to delete ${value}`, item)    
 }
 
 function editItem(e) {
     const item = e.currentTarget.parentElement.parentElement
     // list.removeChild(item)
+}
+
+function showModal(message, item) {
+    function handleYes() {
+        list.removeChild(item)
+        closeModal()
+    }
+
+    function handleNo() {
+        closeModal()
+    }
+
+    function closeModal() {
+        document.querySelector('.modal').remove()
+        postModal()
+        const value = item.querySelector('p').innerText
+        displayMessage(`Item ${value} removed.`, 'danger')
+        if (list.children.length === 0) {
+            itemContainer.classList.remove('show-groceries')
+        }
+    }
+
+    const div = document.createElement('div')
+    div.classList.add('modal')
+    div.innerHTML = `<p>${message}</p><div class="btn-row"><button id="yes">Yes</button><button id="no">No</button></div>`
+    div.classList.add('.toast')
+    document.body.appendChild(div)
+    document.getElementById('yes').addEventListener('click', handleYes)
+    document.getElementById('no').addEventListener('click', handleNo)
+}
+
+function preModal() {
+    submitButton.disabled = true
+    newItem.disabled = true
+    const editButtons = document.querySelectorAll('.btn-edit')
+    const deleteButtons = document.querySelectorAll('.btn-delete')
+    editButtons.forEach( btn => btn.onclick = null)
+    deleteButtons.forEach( btn => btn.onclick = null)
+    const clearButton = document.querySelector('.btn-clear')
+    clearButton.disabled = true
+}
+
+function postModal() {
+    submitButton.disabled = false
+    newItem.disabled = false
+    const editButtons = document.querySelectorAll('.btn-edit')
+    const deleteButtons = document.querySelectorAll('.btn-delete')
+    editButtons.forEach( btn => btn.onclick = editItem)
+    deleteButtons.forEach( btn => btn.onclick = deleteItem)
+    const clearButton = document.querySelector('.btn-clear')
+    clearButton.disabled = false
+}
+
+function reset() {
+
 }
